b3e1567b42ffd6b2ffcb1054c695deeb52109961
david <david@prueba.com>
2025-06-07 23:49:19 -0500

    primer commit base de la aplicacion

---
+++ resources/views/template.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Sistema de gestión de practicantes">
+    <meta name="author" content="DavidJA">
+    <title>UNJFSC</title>
+
+</head>
+
+<body>
+
+    @include('layouts.parte-superior')
+             
+
+          
+        <h1>Aca va el codigo</h1>
+
+
+    @include('layouts.parte-inferior')
+
+</body>
+
+</html>
